2a8a3988464ede414ef7e5cf1d65bcfb1e74bdcf
Derek Leung <derex.leung@gmail.com>
2016-01-21 13:06:35 -0500

    Create Graph.swift

---
+++ contents/Dijkstra-Method/Graph.swift
@@ -0,0 +1,14 @@
+//
+//  Graph.swift
+//  Dijkstra
+//
+//  Adapted from Julien Gomès under Apache License.
+//  Edited by Advanced Computer Science class for educational use.
+//
+
+import Foundation
+
+class Graph {
+    var vertexList: Vertex[] = Vertex[]()
+    var edgeList: Edge[] = Edge[]()
+}
