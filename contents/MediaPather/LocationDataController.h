a550ab1ab6fc0556ba83f37addf473f34451657e
Derek Leung <derex.leung@gmail.com>
2016-05-10 15:05:42 -0400

    Add files via upload

---
+++ contents/MediaPather/LocationDataController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.h
+//  MediaPather
+//
+//  Created by Derek Leung on 5/10/16.
+//  Copyright © 2016 Derek Leung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationDataController : NSObject
+
+@end
---
+++ contents/MediaPather/LocationDataController.m
@@ -0,0 +1,9 @@
+//
+//  LocationDataController.m
+//  MediaPather
+//
+//  Created by Derek Leung on 5/10/16.
+//  Copyright © 2016 Derek Leung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
