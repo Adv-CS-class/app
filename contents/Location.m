c703ce49eb4b69dad2ffb19c90c2bb62464a62c2
Derek Leung <derex.leung@gmail.com>
2016-05-10 15:02:37 -0400

    Uploaded MediaPather files.
    Incomplete

---
+++ contents/Location.m
@@ -0,0 +1,12 @@
+//
+//  Location.m
+//  MediaPather
+//
+//  Created by Derek Leung on 5/10/16.
+//  Copyright © 2016 Derek Leung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@property (strong, nonatomic) NSString *address;
+@property (Strong, nonatomic) NSSTring *PhotoFileName;
---
+++ contents/LocationDataController.m
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.m
+//  MediaPather
+//
+//  Created by Derek Leung on 5/10/16.
+//  Copyright © 2016 Derek Leung. All rights reserved.
+//
+
+#import "LocationDataController.h"
+
+@implementation LocationDataController
+
+@end
